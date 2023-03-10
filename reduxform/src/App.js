eadab51fb9e6e6c63c1fa3e460489761675d3c8d
Deep Prajapati <deep.p@brilworks.com>
2023-03-10 11:10:39 +0530

    code update

---
+++ reduxform/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import './App.css';
+
+function App () {
+    return (
+      <div className="App">
+       <BrowserRouter>
+        <Routes>
+          <Route></Route>
+          <Route></Route>
+        </Routes>
+       </BrowserRouter>
+      </div>
+    );
+}
+
+export default App;
